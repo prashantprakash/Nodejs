5ddf83c35db260bc13c4b365230fc7291998dd80
prashantprakash <prashantpacy@gmail.com>
2014-11-13 22:37:22 -0500

    Simple Chat Application
    Simple Chat Application using socket.io

---
+++ chatrooms/server.js
@@ -0,0 +1,64 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+var socketio = require('socket.io');
+var mime = require('mime');
+var cache = {};
+
+function send404(response) {
+	response.writeHead(404, {'Content-Type': 'text/plain'});
+	response.write('Error 404 : resource not found');
+	response.end();
+}
+
+
+function sendFile(response, filepath ,filecontents) {
+	response.writeHead(200,{'Content-Type' : mime.lookup(path.basename(filepath))});
+	response.end(filecontents);
+}
+
+function serveStatic(response,cache, absPath) {
+	if( cache[absPath]) {
+		sendFile(response, absPath ,cache[absPath]);
+	}else{
+		fs.exists(absPath, function(exists){
+			if(exists){
+				fs.readFile(absPath , function(err,data){
+					if(err) {
+						send404(response);
+					}else{
+						cache[absPath] = data;
+						sendFile(response, absPath, data);
+					}
+				});
+			} else{
+				send404(response);
+			}
+		});
+	}
+}
+
+// Creating a Server 
+
+var server = http.createServer(function(request,response) {
+	var filePath = false;
+	//console.log(request.url);
+	if(request.url == '/'){
+		filePath = 'public/index.html';
+	} else{
+		filePath = 'public' + request.url;
+	}
+	var absPath = './' + filePath;
+	
+	serveStatic(response,cache,absPath);
+});
+
+server.listen(3000, function() {
+	console.log("Server is listening on port no 3000");
+});
+
+var chatserver = require('./lib/chat_server');
+chatserver.listen(server);
+
+
